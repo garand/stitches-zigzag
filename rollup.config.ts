470ca18d8b934ff047a833887667d7e7c4d901ce
Anthony Garand <garand@garand.me>
2021-05-13 11:33:11 -0400

    feat: switch to esbuild and export cjs and esm

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,18 +1,28 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import typescript from "rollup-plugin-typescript2";
-import { terser } from "rollup-plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import esbuild from "rollup-plugin-esbuild";
 
 export default {
   input: "src/index.ts",
   output: [
     {
-      file: "dist/index.js",
+      file: "dist/cjs/index.js",
+      format: "cjs",
+    },
+    {
+      file: "dist/esm/index.js",
       format: "esm",
     },
   ],
   plugins: [
-    peerDepsExternal({ includeDependencies: true }),
-    typescript(),
-    terser(),
+    peerDepsExternal(),
+    resolve({
+      browser: true,
+      preferBuiltins: true,
+      extensions: [".mjs", ".js", "json", ".node", ".jsx", ".ts", ".tsx"],
+    }),
+    commonjs(),
+    esbuild(),
   ],
 };
